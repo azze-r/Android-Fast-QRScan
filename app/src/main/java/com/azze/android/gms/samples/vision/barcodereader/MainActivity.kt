cdf834ab2619210b74dad23105abf77b37178531
azedine <azedine.rouani@gmail.com>
2023-04-10 05:19:51 +0000

    change bar scanner to ML Android and fix design

--- app/src/main/java/com/azze/android/gms/samples/vision/barcodereader/MainActivity.kt
+++ app/src/main/java/com/azze/android/gms/samples/vision/barcodereader/MainActivity.kt
@@ -1,155 +1,83 @@
-/*
- * Copyright (C) The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.azze.android.gms.samples.vision.barcodereader
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.os.Bundle
-import android.app.Activity
 import android.content.Context
-import android.util.Log
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
-
-import com.google.android.gms.common.api.CommonStatusCodes
-import com.google.android.gms.vision.barcode.Barcode
+import android.content.Intent
 import android.net.Uri
-import android.view.animation.AnimationUtils
-import java.lang.Exception
-import androidx.appcompat.app.AlertDialog
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.azz.android.gms.samples.vision.barcodereader.R
-import com.azze.android.gms.samples.vision.barcodereader.ui.barcode.BarcodeCaptureActivity
-import android.animation.Animator
-
-import android.animation.AnimatorListenerAdapter
-
-import android.view.animation.LinearInterpolator
-
-
-
+import com.azz.android.gms.samples.vision.barcodereader.databinding.ActivityMainBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanner
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 
 
 /**
  * Main activity demonstrating how to pass extra parameters to an activity that
  * reads barcodes.
  */
-class MainActivity : Activity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
+    private lateinit var scanner: GmsBarcodeScanner
+    private lateinit var binding: ActivityMainBinding
 
-    private var statusMessage: TextView? = null
-
-    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        findViewById<View>(R.id.read_barcode).setOnClickListener(this)
-
-    }
-
-    /**
-     * Called when a view has been clicked.
-     *
-     * @param v The view that was clicked.
-     */
-    override fun onClick(v: View) {
-        if (v.id == R.id.read_barcode) {
-
-            // launch barcode activity.
-            val intent = Intent(applicationContext, BarcodeCaptureActivity::class.java)
-            intent.putExtra(BarcodeCaptureActivity.AutoFocus, true)
-            //            intent.putExtra(BarcodeCaptureActivity.UseFlash, useFlash.isChecked());
-            startActivityForResult(intent, RC_BARCODE_CAPTURE)
-
-            /*
-            v.animate()
-                .rotationBy(360F)
-                .setDuration(500)
-                .setInterpolator(LinearInterpolator())
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animator: Animator) {
-                        v.rotationX = 0F
-                        v.rotationY = 0F
-
-
-                    }
-                })*/
-
+        scanner = GmsBarcodeScanning.getClient(this)
+        binding.readBarcode.setOnClickListener {
+            launchScan(this)
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == RC_BARCODE_CAPTURE) {
-            if (resultCode == CommonStatusCodes.SUCCESS) {
-                if (data != null) {
-                    val barcode = data.getParcelableExtra<Barcode>(BarcodeCaptureActivity.BarcodeObject)
-                    if (barcode != null) {
-                        Log.i("tryhard", barcode.displayValue)
-                        val builder = AlertDialog.Builder(this,R.style.MyDialogTheme)
-                        builder.setTitle("Code Bar Detected")
-                        builder.setMessage(barcode.displayValue)
-                        // add the buttons
+    fun launchScan(context: Context) {
 
-                        builder.setPositiveButton("Open") { _, _ ->
-                            val url = barcode.displayValue
-                            val i = Intent(Intent.ACTION_VIEW)
-                            i.data = Uri.parse(url)
-                            try {
-                                startActivity(i)
-                            } catch (e: Exception) {
-                                Toast.makeText(this, "can't open this url on browser", Toast.LENGTH_LONG).show()
-                            }
-                        }
+        scanner.startScan()
+            .addOnSuccessListener { barcode ->
+                if (barcode != null)
+                    buildDialog(barcode, context)
+            }
+            .addOnCanceledListener {
+                // Task canceled
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "can't open this url on browser", Toast.LENGTH_LONG).show()
+            }
 
-                        builder.setNeutralButton("share") { _, _ ->
-                            // do something like...
-                            val sendIntent: Intent = Intent().apply {
-                                action = Intent.ACTION_SEND
-                                putExtra(Intent.EXTRA_TEXT, barcode.displayValue)
-                                type = "text/plain"
-                            }
-                            val shareIntent = Intent.createChooser(sendIntent, null)
-                            startActivity(shareIntent)
-                        }
 
-                        // create and show the alert dialog
-                        val dialog = builder.create()
-                        dialog.show()
-                    } else {
-                        statusMessage?.setText(R.string.barcode_failure)
-                        Log.d(TAG, "No barcode captured, intent data is null")
-                    }
-                } else {
-                    statusMessage?.text = String.format(getString(R.string.barcode_error),
-                            CommonStatusCodes.getStatusCodeString(resultCode))
+
+    }
+
+    
+    private fun buildDialog(barcode: Barcode, context:Context){
+
+        MaterialAlertDialogBuilder(context)
+            .setTitle(resources.getString(R.string.title))
+            .setMessage(barcode.displayValue.toString())
+            .setNeutralButton(resources.getString(R.string.decline)) { _, _ ->
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, barcode.displayValue)
+                    type = "text/plain"
+                }
+
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+            }
+            .setPositiveButton(resources.getString(R.string.accept)) { _, _ ->
+                val url = barcode.displayValue
+                val i = Intent(Intent.ACTION_VIEW)
+                i.data = Uri.parse(url)
+                try {
+                    startActivity(i)
+                } catch (e: Exception) {
+                    Toast.makeText(this, "can't open this url on browser", Toast.LENGTH_LONG).show()
                 }
             }
-            else{
-                Toast.makeText(this, "code bar null", Toast.LENGTH_LONG).show()
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
-    }
-
-    companion object {
-
-        private val RC_BARCODE_CAPTURE = 9001
-        private val TAG = "BarcodeMain"
+            .show()
     }
 }
